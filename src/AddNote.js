d77fd70653e92d625fd8152f6f7cb18f771d34d4
Steven Bull <stevenwbulljr@gmail.com>
2019-09-14 11:47:43 -0500

    moved Error Boundary to a more relevant position in order to handle 3rd party API resolution data

--- src/AddNote.js
+++ src/AddNote.js
@@ -5,104 +5,104 @@ import cuid from 'cuid';
 
 export default class AddNote extends Component {
 
-    state = {
-        name: '',
-        content: '',
-        folderId: '',
-        modified: ''
-    }
+  state = {
+    name: '',
+    content: '',
+    folderId: '',
+    modified: ''
+  }
 
-    static contextType = ApiContext;
+  static contextType = ApiContext;
 
-    handleAddNote = (e) => {
-        e.preventDefault();
-        console.log(this.state.name);
+  handleAddNote = (e) => {
+    e.preventDefault();
+    console.log(this.state.name);
 
-        const newNote = {
-            id: cuid(),
-            name: this.state.name,
-            modified: this.state.modified,
-            content: this.state.content,
-            folderId: this.state.folderId
-        };
-        console.log(newNote)
-
-        fetch(`${config.API_ENDPOINT}/notes`, {
-            method: "POST",
-            headers: {
-                "content-type": "application/json"
-            },
-            body: JSON.stringify(newNote)
-        })
-            .then(res => {
-                if (!res.ok) return res.json().then(e => Promise.reject(e));
-                return res.json();
-            })
-            .then(() => {
-                this.context.addNote(newNote);
-                this.props.history.push('/');
-            })
-            .catch(error => {
-                console.error({ error });
-            });
+    const newNote = {
+      id: cuid(),
+      name: this.state.name,
+      modified: this.state.modified,
+      content: this.state.content,
+      folderId: this.state.folderId
     };
+    console.log(newNote)
 
-    validateNoteName = () => {
-        let note = this.state.name;
+    fetch(`${config.API_ENDPOINT}/notes`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(newNote)
+    })
+      .then(res => {
+        if (!res.ok) return res.json().then(e => Promise.reject(e));
+        return res.json();
+      })
+      .then(() => {
+        this.context.addNote(newNote);
+        this.props.history.push('/');
+      })
+      .catch(error => {
+        console.error({ error });
+      });
+  };
 
-        if (!note) {
-            return 'Note name is required'
-        } else {
-            return null
-        }
+  validateNoteName = () => {
+    let note = this.state.name;
+
+    if (!note) {
+      return 'Note name is required'
+    } else {
+      return null
     }
+  }
 
-    validateContent = () => {
-        let note = this.state.content;
+  validateContent = () => {
+    let note = this.state.content;
 
-        if (!note) {
-            return 'Note content is required'
-        } else {
-            return null
-        }
+    if (!note) {
+      return 'Note content is required'
+    } else {
+      return null
     }
+  }
 
-    validateFolder = () => {
-        let folder = this.state.folderId;
+  validateFolder = () => {
+    let folder = this.state.folderId;
 
-        if (!folder) {
-            return 'Note content is required'
-        } else {
-            return null
-        }
+    if (!folder) {
+      return 'Note content is required'
+    } else {
+      return null
     }
- 
-    render() {
-        const { folders } = this.context;
+  }
 
-        return (
-            <form onSubmit={e => this.handleAddNote(e)}>
-                <div>
-                    <label htmlFor='noteName'>New Note Name: </label>
-                    <input type='text' id='noteName' value={this.state.name} onChange={ e => this.setState({name: e.target.value})} />
-                    {this.validateNoteName && <p>{this.validateNoteName()}</p>}
-                </div>
-                <div>
-                    <label htmlFor='noteContent'>Content: </label>
-                    <input type='text' id='noteName' value={this.state.content} onChange={ e => this.setState({content: e.target.value})} />
-                    {this.validateContent && <p>{this.validateContent()}</p>}
-                </div>
-                <div>
-                    <select name='Choose folder...' value={this.state.folderId} onChange={ e => this.setState({folderId: e.target.value})}>
-                        <option key="default" value={null}>Select folder</option>
-                        {folders.map((folder) => <option key={folder.id} value={folder.id}>{folder.name}</option>)}
-                    </select>
-                    {this.validateFolder && <p>{this.validateFolder()}</p>}
-                </div>
-                <div>
-                    <button disabled={this.validateNoteName() || this.validateContent() || this.validateFolder()} type='submit' onClick={ e => this.setState({modified: new Date().toLocaleString()})}>Submit</button>
-                </div>
-            </form>
-        );
-    }
+  render() {
+    const { folders } = this.context;
+
+    return (
+      <form onSubmit={e => this.handleAddNote(e)}>
+        <div>
+          <label htmlFor='noteName'>New Note Name: </label>
+          <input type='text' id='noteName' value={this.state.name} onChange={e => this.setState({ name: e.target.value })} />
+          {this.validateNoteName && <p>{this.validateNoteName()}</p>}
+        </div>
+        <div>
+          <label htmlFor='noteContent'>Content: </label>
+          <input type='text' id='noteName' value={this.state.content} onChange={e => this.setState({ content: e.target.value })} />
+          {this.validateContent && <p>{this.validateContent()}</p>}
+        </div>
+        <div>
+          <select name='Choose folder...' value={this.state.folderId} onChange={e => this.setState({ folderId: e.target.value })}>
+            <option key="default" value={null}>Select folder</option>
+            {folders.map((folder) => <option key={folder.id} value={folder.id}>{folder.name}</option>)}
+          </select>
+          {this.validateFolder && <p>{this.validateFolder()}</p>}
+        </div>
+        <div>
+          <button disabled={this.validateNoteName() || this.validateContent() || this.validateFolder()} type='submit' onClick={e => this.setState({ modified: new Date().toLocaleString() })}>Submit</button>
+        </div>
+      </form>
+    );
+  }
 }
--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
-import {Route, Link} from 'react-router-dom';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import NoteListNav from '../NoteListNav/NoteListNav';
 import NotePageNav from '../NotePageNav/NotePageNav';
 import NoteListMain from '../NoteListMain/NoteListMain';
@@ -14,118 +14,118 @@ import ErrorPage from '../ErrorPage'
 import './App.css';
 
 class App extends Component {
-    state = {
-        notes: [],
-        folders: []
+  state = {
+    notes: [],
+    folders: []
+  };
+
+  componentDidMount() {
+    Promise.all([
+      fetch(`${config.API_ENDPOINT}/notes`),
+      fetch(`${config.API_ENDPOINT}/folders`)
+    ])
+      .then(([notesRes, foldersRes]) => {
+        if (!notesRes.ok)
+          return notesRes.json().then(e => Promise.reject(e));
+        if (!foldersRes.ok)
+          return foldersRes.json().then(e => Promise.reject(e));
+
+        return Promise.all([notesRes.json(), foldersRes.json()]);
+      })
+      .then(([notes, folders]) => {
+        this.setState({ notes, folders });
+      })
+      .catch(error => {
+        console.error({ error });
+      });
+  }
+
+  handleDeleteNote = noteId => {
+    this.setState({
+      notes: this.state.notes.filter(note => note.id !== noteId)
+    });
+  };
+
+  addNewFolder = (newFolder) => {
+    this.setState({
+      folders: [
+        ...this.state.folders,
+        newFolder
+      ]
+    })
+  }
+
+  addNewNote = (newNote) => {
+    this.setState({
+      notes: [
+        ...this.state.notes,
+        newNote
+      ]
+    })
+  }
+
+  renderNavRoutes() {
+    return (
+      <>
+        {['/', '/folder/:folderId'].map(path => (
+          <Route
+            exact
+            key={path}
+            path={path}
+            component={NoteListNav}
+          />
+        ))}
+        <Route path="/note/:noteId" component={NotePageNav} />
+        <Route path="/add-folder" component={NotePageNav} />
+        <Route path="/add-note" component={NotePageNav} />
+      </>
+    );
+  }
+
+  renderMainRoutes() {
+    return (
+      <>
+        {['/', '/folder/:folderId'].map(path => (
+          <Route
+            exact
+            key={path}
+            path={path}
+            component={NoteListMain}
+          />
+        ))}
+        <Route path="/note/:noteId" component={NotePageMain} />
+        <Route path='/add-folder' component={AddFolder} />
+        <Route path='/add-note' component={AddNote} />
+      </>
+    );
+  }
+
+  render() {
+    const value = {
+      notes: this.state.notes,
+      folders: this.state.folders,
+      deleteNote: this.handleDeleteNote,
+      addFolder: this.addNewFolder,
+      addNote: this.addNewNote
+
     };
-
-    componentDidMount() {
-        Promise.all([
-            fetch(`${config.API_ENDPOINT}/notes`),
-            fetch(`${config.API_ENDPOINT}/folders`)
-        ])
-            .then(([notesRes, foldersRes]) => {
-                if (!notesRes.ok)
-                    return notesRes.json().then(e => Promise.reject(e));
-                if (!foldersRes.ok)
-                    return foldersRes.json().then(e => Promise.reject(e));
-
-                return Promise.all([notesRes.json(), foldersRes.json()]);
-            })
-            .then(([notes, folders]) => {
-                this.setState({notes, folders});
-            })
-            .catch(error => {
-                console.error({error});
-            });
-    }
-
-    handleDeleteNote = noteId => {
-        this.setState({
-            notes: this.state.notes.filter(note => note.id !== noteId)
-        });
-    };
-
-    addNewFolder = (newFolder) => {
-        this.setState({
-            folders: [
-            ...this.state.folders,
-            newFolder
-            ]
-        })
-    }
-
-    addNewNote = (newNote) => {
-        this.setState({
-            notes: [
-                ...this.state.notes,
-                newNote
-            ]
-        })
-    }
-
-    renderNavRoutes() {
-        return (
-            <>
-                {['/', '/folder/:folderId'].map(path => (
-                    <Route
-                        exact
-                        key={path}
-                        path={path}
-                        component={NoteListNav}
-                    />
-                ))}
-                <Route path="/note/:noteId" component={NotePageNav} />
-                <Route path="/add-folder" component={NotePageNav} />
-                <Route path="/add-note" component={NotePageNav} />
-            </>
-        );
-    }
-
-    renderMainRoutes() {
-        return (
-            <>
-                {['/', '/folder/:folderId'].map(path => (
-                    <Route
-                        exact
-                        key={path}
-                        path={path}
-                        component={NoteListMain}
-                    />
-                ))}
-                <Route path="/note/:noteId" component={NotePageMain} />
-                <Route path='/add-folder' component={AddFolder} />
-                <Route path='/add-note' component={AddNote} />
-            </>
-        );
-    }
-
-    render() {
-        const value = {
-            notes: this.state.notes,
-            folders: this.state.folders,
-            deleteNote: this.handleDeleteNote,
-            addFolder: this.addNewFolder,
-            addNote: this.addNewNote
-            
-        };
-        return (
-            <ErrorPage>
-                <ApiContext.Provider value={value}>
-                    <div className="App">
-                        <nav className="App__nav">{this.renderNavRoutes()}</nav>
-                        <header className="App__header">
-                            <h1>
-                                <Link to="/">Noteful</Link>{' '}
-                                <FontAwesomeIcon icon="check-double" />
-                            </h1>
-                        </header>
-                        <main className="App__main">{this.renderMainRoutes()}</main>
-                    </div>
-                </ApiContext.Provider>
-            </ErrorPage>
-        );
-    }
+    return (
+      <ApiContext.Provider value={value}>
+        <div className="App">
+          <nav className="App__nav">{this.renderNavRoutes()}</nav>
+          <header className="App__header">
+            <h1>
+              <Link to="/">Noteful</Link>{' '}
+              <FontAwesomeIcon icon="check-double" />
+            </h1>
+          </header>
+          <ErrorPage>
+            <main className="App__main">{this.renderMainRoutes()}</main>
+          </ErrorPage>
+        </div>
+      </ApiContext.Provider>
+    );
+  }
 }
 
 export default App;
